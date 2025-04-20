e3c98d3db625dbb5f5fae0b33cff183e2ffac787
Nalin337 <nalinsrivastava.nks@gmail.com>
2025-04-20 11:46:57 +0530

    Add files via upload

---
+++ 12B.cpp
@@ -0,0 +1,28 @@
+// NAME - NALIN KUMAR SRIVASTAVA
+// PRN - 23070123157
+// EXPERIMENT - 12(B) 
+
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater.";
+        }
+        else 
+        {
+            cout<<d<<" is greater.";
+        }
+    }
+};
+int main()
+{
+    Num n1(4,3);
+} 
+
+// Output 
+// 4 is greater. 
